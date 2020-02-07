7c6a7473b8e0468fe2559d8ec08a5d1e0f681ded
zhsss <48230390+zhsss@users.noreply.github.com>
2020-02-07 22:45:35 +0800

    Add files via upload

---
+++ quickunion.java
@@ -0,0 +1,32 @@
+package suanfajava;
+
+public class quickunion {
+
+	private int [] id;
+	
+	public void QuickFindUF(int N) {
+		id=new int[N];
+		for(int i=0;i<N;i++) {
+			id[i]=i;
+		}
+	}
+	
+	public int root(int i) {
+		while(i!=id[i]) {
+			i=id[i];
+		}
+		return i;
+		
+	}
+	public boolean connected(int p,int q) {
+		return id[p]==id[q];
+		
+	}
+	
+	public void union(int p,int q) {
+		int pid=root(p);
+		int qid=root(q);
+		id[pid]=qid;
+		
+	}
+}
