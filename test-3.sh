6fe4fde41a0f18490d3e8eb3413a1299ec4231d8
zhsss <48230390+zhsss@users.noreply.github.com>
2020-10-19 19:06:37 +0800

    Update test-3.sh

--- test-3.sh
+++ test-3.sh
@@ -14,3 +14,35 @@ grep -oP '\s+4\d{2}\s+(?=\d+)' web_log.tsv | sort -u | xargs -i sh -c "grep  -P 
 echo "-------------------------------------------------------------------"
 grep -P $1'\s+' web_log.tsv | awk '{printf "%s \n",$1}'| sort | uniq -c | sort -nr -k1 | head -10
 echo "-------------------------------------------------------------------"
+
+
+#import <Foundation/Foundation.h> 
+
+int main()
+{
+   NSString *str0=@"文艺青年";
+ 
+    NSString *temp = @"文艺";
+    NSRange rang = [str0 rangeOfString:temp];
+ 
+    NSLog(@"搜索的字符串在str0中起始点的index 为 %d", rang.location);
+    NSLog(@"搜索的字符串在str0中结束点的index 为 %d", rang.location + rang.length);
+ 
+    //将搜索中的字符串替换成为一个新的字符串
+    NSString *str = [str0 stringByReplacingCharactersInRange:rang withString:@"123"];
+    NSLog(@"替换后字符串为%@", str);
+    NSString *str1=@"i love you";
+    NSString *capitalize=[str1 capitalizedString];
+    NSLog(@"str1首字母大写:%@",capitalize);
+    
+    NSString *str3=@"20|https://www.cuc.edu.cn”https://www.cuc.edu.cn";
+    NSString *temp1=@"|";
+    
+    NSRange rang1=[str3 rangeOfString:temp1];
+    int point=rang1.location;
+    NSString * to = [str3 substringToIndex:point];
+    NSString * from=[str3 substringFromIndex:point+1];
+    NSLog(@"to = %@",to);
+    NSLog(@"from = %@",from);
+   return 0;
+}
